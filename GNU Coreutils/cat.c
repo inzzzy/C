bd497fc1567d18f56f08faeb67296829a9177180
Roko <83535959+inzzzy@users.noreply.github.com>
2021-10-22 15:47:55 +0200

    Create cat.c

---
+++ GNU Coreutils/cat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*  Simple implementation of the GNU Coreutils Cat program.
+        Inputs from filestream and prints to stdout.      */
+
+void concatenate(FILE *from, FILE *to){
+    int c;
+    while ( (c = getc(from)) != EOF )
+        fprintf(to, "%c", c);
+}
+
+int main(int argc, char *argv[]){
+    FILE *fp;
+
+    if (argc == 1)
+        concatenate(stdin, stdout);
+    else
+        while(--argc > 0)
+            if ((fp = fopen(*++argv, "r")) == NULL){
+                fprintf(stderr, "ERROR: Can not open file %s\n", *argv);
+                exit(1);
+            }
+            else{
+                concatenate(fp, stdout);
+                fclose(fp);
+            }
+
+    return 0;
+}
+
+
